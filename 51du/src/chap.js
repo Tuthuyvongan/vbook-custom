a1f35f376dc121517f45592432948e046b7c4531
Tuthuyvongan <48273012+Tuthuyvongan@users.noreply.github.com>
2022-10-13 13:29:57 +0700

    test

--- 51du/src/chap.js
+++ 51du/src/chap.js
@@ -1,12 +1,5 @@
 function execute(url) {
-  url = url.replace(
-    "https://m.51du.org",
-    "https://www.51du.org",
-    "http://www.51du.org",
-    "http://m.51du.org",
-    "m.51du.org",
-    "www.51du.org"
-  );
+  url = url.replace("www.51du.org", "");
   let response = fetch(url);
 
   if (response.ok) {

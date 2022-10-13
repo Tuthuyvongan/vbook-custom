a8cad0cfd432b1d2de459f5d15d4a3651f503574
Tuthuyvongan <48273012+Tuthuyvongan@users.noreply.github.com>
2022-10-13 13:03:26 +0700

    test

--- 51du/src/detail.js
+++ 51du/src/detail.js
@@ -1,14 +1,18 @@
 function execute(url) {
   url = url.replace("m.51du.org", "www.51du.org");
   let response = fetch(url);
-  let novelTags;
   if (response.ok) {
     let doc = response.html();
     let coverImg = doc.select(".jieshao .lf img").first().attr("src");
     if (coverImg.startsWith("//")) {
       coverImg = "http:" + coverImg;
     }
-
+    let novelTags = doc.select(".info a").text();
+    let author = doc
+      .select(".msg em")
+      .first()
+      .text()
+      .replace(/作\s*者：/g, "");
     return Response.success({
       name: doc.select("h1").text(),
       cover: coverImg,

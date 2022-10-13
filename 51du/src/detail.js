2e1811a624e15ac00c5d0bef42d702282cdea8b1
Tuthuyvongan <48273012+Tuthuyvongan@users.noreply.github.com>
2022-10-13 13:06:40 +0700

    fix

--- 51du/src/detail.js
+++ 51du/src/detail.js
@@ -7,7 +7,7 @@ function execute(url) {
     if (coverImg.startsWith("//")) {
       coverImg = "http:" + coverImg;
     }
-    let novelTags = doc.select(".info a").text();
+    let novelTags = doc.select(".msg em").get(1).text();
     let author = doc
       .select(".msg em")
       .first()
@@ -16,13 +16,9 @@ function execute(url) {
     return Response.success({
       name: doc.select("h1").text(),
       cover: coverImg,
-      author: doc
-        .select(".msg em")
-        .first()
-        .text()
-        .replace(/作\s*者：/g, ""),
+      author: author,
       description: doc.select(".jieshao p").text(),
-      novelTags: doc.select(".info a").text(),
+      novelTags: novelTags,
       detail: "作者： " + author + "<br>" + novelTags,
       host: "http://www.51du.org",
     });
